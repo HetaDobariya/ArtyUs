cc2a7f79c12175295a003f8fcbd053a4db4ef35b
Om Somani <omsomani789@gmail.com>
2025-11-03 18:19:29 +0530

    auth backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app= express();
 const PORT = process.env.PORT;
+
+app.use(cookieParser());
 
 app.use(express.json());
 app.use('/api/user',userRoutes);
